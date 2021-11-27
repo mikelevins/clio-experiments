cb8ea83d083c262d559c4e8e450231ed6c4ed83f
mikel evins <mikel@evins.net>
2021-11-27 11:49:15 -0600

    modified boot.html to load codemirror

--- src/ui.lisp
+++ src/ui.lisp
@@ -5,11 +5,9 @@
   (setf trivial-open-browser::+format-string+ "start msedge ~S"))
 
 (defun on-new-window (body)
-  (load-script (html-document body) "https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.64.0/codemirror.min.js")
-  (load-css (html-document body) "https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.64.0/codemirror.min.css")
-  (load-script (html-document body) "https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.64.0/keymap/emacs.min.js")
-  (load-script (html-document body) "https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.64.0/mode/commonlisp/commonlisp.min.js")
-  ;;(js-execute (html-document body) (format nil "var myCodeMirror = CodeMirror(document.body, {mode: 'commonlisp'});"))
+  (js-execute
+   (html-document body)
+   (format nil "var myCodeMirror = CodeMirror(document.body, {mode: 'commonlisp', keyMap: 'emacs'});"))
   )
 
 (defun start ()
