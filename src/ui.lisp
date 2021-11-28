a96591bb7941c8df6735865935ba3f51b8daae32
mikel evins <mikel@evins.net>
2021-11-28 10:01:58 -0600

    added jquery-terminal

--- src/ui.lisp
+++ src/ui.lisp
@@ -7,7 +7,34 @@
 (defun on-new-window (body)
   (js-execute
    (html-document body)
-   (format nil "var TheCodeMirror = CodeMirror(document.body, {mode: 'commonlisp', keyMap: 'emacs', lineNumbers: true});")))
+   (format nil "
+var worksheet = document.getElementById('worksheet');
+var output = document.getElementById('output');
+var TheCodeMirror = CodeMirror(worksheet, {mode: 'commonlisp', keyMap: 'emacs', lineNumbers: true});
+
+$('#output').terminal({
+    add: function(a, b) {
+        this.echo(a + b);
+    },
+    re: function(re, str) {
+       if (re instanceof RegExp && re.test(str)) {
+          this.echo(str + ' [[;green;]match]');
+       }
+    },
+    foo: 'foo.php',
+    bar: {
+        sub: function(a, b) {
+            this.echo(a - b);
+        }
+    }
+}, {
+    greetings: '',
+    height: 300,
+    width: '100%',
+    prompt: '> '
+});
+
+")))
 
 (defun start ()
   (initialize #'on-new-window :static-root #P"www/")
