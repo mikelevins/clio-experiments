3a966a2356a4ce6cceeb0e364f28576d96ddede2
mikel evins <mikel@evins.net>
2021-12-17 21:53:50 -0600

    added app flag when launching edge

--- src/ui.lisp
+++ src/ui.lisp
@@ -1,7 +1,7 @@
 (in-package :clio)
 
 (defparameter +open-browser-command+
-  #+(or win32 mswindows windows) "start msedge ~S"
+  #+(or win32 mswindows windows) "start msedge --app=~S"
   #+(or macos darwin) "open ~S"
   #-(or win32 mswindows macos darwin windows) "xdg-open ~S")
 
